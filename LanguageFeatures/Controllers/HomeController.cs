690dc8ae3e5a8eedf1b650802bc3c7d2d42f32a9
Thiago Reis <thiagoreis128@gmail.com>
2017-09-22 10:11:35 -0300

    Creating ojb propertys

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
@@ -11,6 +12,18 @@ namespace LanguageFeatures.Controllers
         public string Index()
         {
             return "Navigate to URL to...";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            
+            Product myProduct = new Product();
+            myProduct.Name = "Teste";
+
+            string productName = myProduct.Name;
+
+            return View("Result", (object) String.Format("Product name: {0}", productName));
+
         }
 
         public ActionResult About()
