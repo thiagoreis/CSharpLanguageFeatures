b7d8e4d10f895c4edd951479f6daee3d39d6957f
Thiago Reis <thiagoreis128@gmail.com>
2017-09-22 09:58:49 -0300

    Add project files.

---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        private string Name { get; set; }
+
+
+    }
+}
