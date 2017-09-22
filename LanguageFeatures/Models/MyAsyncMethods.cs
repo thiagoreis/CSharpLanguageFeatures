53d2887aa8be8d01fc7f26ef7f224843c2babc39
Thiago Reis <thiagoreis128@gmail.com>
2017-09-22 14:30:55 -0300

    Using Async Methods

---
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+        public static Task<long?> GetPageLength()
+        {
+            HttpClient cliente = new HttpClient();
+            var httpTask = cliente.GetAsync("http://apress.com");
+
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) => {
+                return antecedent.Result.Content.Headers.ContentLength;
+            });
+        }
+    }
+}
